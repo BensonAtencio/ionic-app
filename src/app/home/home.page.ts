ec83e478a3d830f2e78a3e3e61ba0740e4829d36
BensonAtencio <benson.atencio@g.batstate-u.edu.ph>
2022-07-26 16:22:51 +0800

    adding of action sheet

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { PopoverController, ModalController } from '@ionic/angular';
+import { PopoverController, ActionSheetController } from '@ionic/angular';
 import { PopoverComponent } from '../components/popover/popover.component';
 
 
@@ -10,7 +10,7 @@ import { PopoverComponent } from '../components/popover/popover.component';
 })
 export class HomePage {
 
-  constructor(public popoverCtrl: PopoverController) {}
+  constructor(public popoverCtrl: PopoverController, public actionSheetCtrl: ActionSheetController) {}
 
   async menu(event){
     const popover = await this.popoverCtrl.create({
@@ -19,6 +19,44 @@ export class HomePage {
     });
     return await popover.present();
 
+  }
+
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      cssClass: 'my-custom-class',
+      buttons: [{
+        text: 'Delete',
+        icon: 'trash',
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Share',
+        icon: 'share',
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Play (open modal)',
+        icon: 'caret-forward-circle',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
   }
 
 }
